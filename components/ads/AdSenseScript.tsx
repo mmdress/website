42489a41d5d39b41f9985ce46edeb4892fbe6e68
Lucas Cavalcante <lucas.cavalcante.dev@gmail.com>
2026-05-25 20:47:15 -0300

    refactor: replace Next.js Script component with native script tag in AdSenseScript for improved performance

--- components/ads/AdSenseScript.tsx
+++ components/ads/AdSenseScript.tsx
@@ -1,5 +1,3 @@
-import Script from 'next/script';
-
 const ADSENSE_CLIENT_ID = process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID;
 
 export function AdSenseScript() {
@@ -8,11 +6,10 @@ export function AdSenseScript() {
   }
 
   return (
-    <Script
+    <script
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_CLIENT_ID}`}
       crossOrigin="anonymous"
-      strategy="afterInteractive"
     />
   );
 }
